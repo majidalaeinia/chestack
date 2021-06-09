d861515077b82af465eb105b3d78e6f152e8253e
Majid Alaeinia <alaeinia.majid@gmail.com>
2021-06-09 17:08:10 +0430

    Run database on public

--- public/start.go
+++ public/start.go
@@ -2,12 +2,14 @@ package public
 
 import (
 	"github.com/MajidAlaeinia/chestack/app/providers"
+	"github.com/MajidAlaeinia/chestack/database"
 	"github.com/spf13/viper"
 	"log"
 	"os"
 )
 
 func Run() {
+	database.Run()
 	r := providers.Routes()
 
 	err := r.Run(viper.GetString("GIN.ADDRESS"))
