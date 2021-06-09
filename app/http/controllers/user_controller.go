45190a09bb5ec41a580ce3f84df3a56ff253ee6e
Majid Alaeinia <alaeinia.majid@gmail.com>
2021-06-09 17:11:17 +0430

    Create 'user_controller'

---
+++ app/http/controllers/user_controller.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"github.com/MajidAlaeinia/chestack/models"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"log"
+	"net/http"
+)
+
+type UserController struct{}
+
+func (ctrl UserController) Show(c *gin.Context) {
+	id := cast.ToInt(c.Param("id"))
+	log.Println(id)
+	usr, err := models.Users(qm.Where("id = ?", id)).OneG()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound,
+			gin.H{
+				"status":  false,
+				"message": "No records found.",
+			})
+		return
+	}
+	c.JSON(http.StatusOK, usr)
+	return
+}
