6275f7bd96eda337c0058ac5f4e94a6379036cb9
Majid Alaeinia <alaeinia.majid@gmail.com>
2021-06-11 03:51:39 +0430

    Refactor 'Show' method

--- app/http/controllers/user_controller.go
+++ app/http/controllers/user_controller.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
-	"github.com/MajidAlaeinia/chestack/models"
+	"context"
+	"fmt"
+	"github.com/MajidAlaeinia/chestack/app/http/resources"
+	"github.com/MajidAlaeinia/chestack/modext"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cast"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"github.com/volatiletech/sqlboiler/v4/queries"
 	"net/http"
 )
 
@@ -12,15 +15,17 @@ type UserController struct{}
 
 func (ctrl UserController) Show(c *gin.Context) {
 	id := cast.ToInt(c.Param("id"))
-	usr, err := models.Users(qm.Where("id = ?", id)).OneG()
+	var user resources.User
+	err := queries.RawG(fmt.Sprintf("SELECT %s FROM `user` WHERE id = %d",
+		modext.VisibleFieldsForRawSelect(), id)).BindG(context.Background(), &user)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound,
 			gin.H{
 				"status":  false,
-				"message": "No records found.",
+				"message": "No records was found.", //TODO: localization.
 			})
 		return
 	}
-	c.JSON(http.StatusOK, usr)
+	c.JSON(http.StatusOK, user)
 	return
 }
