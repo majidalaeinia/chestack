3c828070b35f338ddb6057141dd10d22331ed444
Majid Alaeinia <alaeinia.majid@gmail.com>
2021-06-12 16:18:46 +0430

    Add 'RunTxDb'

--- utils/test.go
+++ utils/test.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/DATA-DOG/go-txdb"
 	"github.com/MajidAlaeinia/chestack/config"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
 	"github.com/volatiletech/sqlboiler/v4/boil"
@@ -28,6 +31,24 @@ func RunDatabase() *sqlx.DB {
 	db.SetMaxOpenConns(viper.GetInt("DB.MAX_OPEN_CONNS"))
 	db.SetMaxIdleConns(viper.GetInt("DB.MAX_IDLE_CONNS"))
 	db.SetConnMaxLifetime(viper.GetDuration("DB.CONN_MAX_LIFE_TIME") * time.Minute)
+
+	return db
+}
+
+func RunTxDb() *sql.DB {
+	config.ReadConfig()
+	txdb.Register("txdb", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?autocommit=true&parseTime=true&loc=%s",
+		viper.GetString("DB.USERNAME"),
+		viper.GetString("DB.PASSWORD"),
+		viper.GetString("DB.HOST"),
+		viper.GetString("DB.PORT"),
+		viper.GetString("DB.NAME"),
+		"Asia%2FTehran"))
+	db, err := sql.Open("txdb", "identifier")
+	if err != nil {
+		log.Fatal(err)
+	}
+	boil.SetDB(db)
 
 	return db
 }
